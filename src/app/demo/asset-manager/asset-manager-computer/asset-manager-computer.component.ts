418ee11c8de6b8957c45cc7bb34b512c4ab9c6ff
Ryoshi <fischerklon3@gmail.com>
2023-12-26 14:08:23 +0100

    Added Paginator to Table

--- src/app/demo/asset-manager/asset-manager-computer/asset-manager-computer.component.ts
+++ src/app/demo/asset-manager/asset-manager-computer/asset-manager-computer.component.ts
@@ -1,46 +1,52 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {Computer} from "../model/computer/computer";
 import {ComputerService} from "../model/computer/computer.service";
 import {HttpErrorResponse} from "@angular/common/http";
 import {NgForOf} from "@angular/common";
 import {NgxPaginationModule} from "ngx-pagination";
+import {MatTableDataSource, MatTableModule} from "@angular/material/table";
+import {MatPaginator, MatPaginatorModule, PageEvent} from "@angular/material/paginator";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-asset-manager-computer',
   standalone: true,
   imports: [
     NgForOf,
-    NgxPaginationModule
+    NgxPaginationModule,
+    MatTableModule,
+    MatPaginatorModule
   ],
   templateUrl: './asset-manager-computer.component.html',
   styleUrl: './asset-manager-computer.component.css'
 })
 export class AssetManagerComputerComponent implements OnInit{
+
   public computers: Computer[];
 
-  ngOnInit(): void {
-    this.getComputer();
-  }
+  public columnsToDisplay: string[] = ["name","serial-number","manufacturer"];
+  dataSource = new MatTableDataSource<Computer>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private computerService: ComputerService) {
     this.computers = [];
   }
 
-  public getComputer():void{
+  ngOnInit(): void {
+    this.getComputer();
+  }
+
+  public getComputer():void {
     this.computerService.getComputer().subscribe( //Returns an Observable and we use subscribe so we grab the data if there appears data
       (response: Computer[]) => {
         this.computers = response;
+        this.dataSource.data = this.computers; // Set data for MatTableDataSource
+        this.dataSource.paginator = this.paginator;
       },
       (error: HttpErrorResponse) => {
         alert(error.message);
       }
     );
   }
-
-  page = 1;
-
-  public handlePageChange(event:number){
-    this.page = event;
-  }
-
 }
