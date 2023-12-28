51c3c80ce10d76f6b387de6d7464f7bf75a09fa2
Ryoshi <dominik.fischer@cewe.de>
2023-12-28 14:11:05 +0100

    Owner in Computer Add Window

--- src/app/demo/asset-manager/asset-manager-navbar/asset-manager-navbar-add/asset-manager-navbar-add.component.ts
+++ src/app/demo/asset-manager/asset-manager-navbar/asset-manager-navbar-add/asset-manager-navbar-add.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, NgForm, ReactiveFormsModule} from "@angular/forms";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from '@angular/material/input';
 import {MatGridListModule} from "@angular/material/grid-list";
+import {ComputerService} from "../../model/computer/computer.service";
+import {Computer} from "../../model/computer/computer";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-asset-manager-navbar-add',
@@ -11,11 +14,27 @@ import {MatGridListModule} from "@angular/material/grid-list";
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatGridListModule
+    MatGridListModule,
+    ReactiveFormsModule
   ],
   templateUrl: './asset-manager-navbar-add.component.html',
   styleUrl: './asset-manager-navbar-add.component.css'
 })
 export class AssetManagerNavbarAddComponent {
+
+  constructor(private computerService: ComputerService) { }
+
+  //TODO: IMPLEMENT OWNER DROPDOWN IN COMPUTER OWNER SELECTION
+
+  public onAddComputer(addComputerForm: NgForm) {
+    this.computerService.addComputer(addComputerForm.value).subscribe(
+      (response: Computer) => {
+        console.log(response)
+      },
+    (error: HttpErrorResponse) => {
+        alert(error.message)
+        }
+    );
+  }
 
 }
